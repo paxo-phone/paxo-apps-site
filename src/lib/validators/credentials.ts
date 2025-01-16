50888ea1233e70845d8dedca734651cc3dc80a85
Raphael Roumezin <raphael.roumezin@ik.me>
2025-01-16 20:49:38 +0100

    Created validator logic (not implemented yet)

---
+++ src/lib/validators/credentials.ts
@@ -0,0 +1,15 @@
+import validator from 'validator'
+
+export function validateUsername(username: string): boolean {
+    return username.length >= 3 && username.length <= 20;
+}
+
+export function validatePassword(password: string): boolean {
+    return validator.isStrongPassword(password, {
+        minLength: 8
+    });
+}
+
+export function validateEmail(email: string): boolean {
+    return validator.isEmail(email);
+}
